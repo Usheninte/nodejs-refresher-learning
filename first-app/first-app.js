f90b8556cb1f155a758f364d37f1438f26e1e855
Usheninte <jrandangana@gmail.com>
2019-02-15 17:42:44 +0100

    initital commit

---
+++ first-app/first-app.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+// `fs` stands for file system
+// this enables us to use some file system functionality within this file
+
+fs.writeFileSync('hello.txt', 'Hello from Node.js ');
