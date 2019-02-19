83e1999621a4c80ab4c3074a7d3300f976e48237
Usheninte <jrandangana@gmail.com>
2019-02-19 18:46:55 +0100

    using the node modules system

--- first-node-server/routes.js
+++ first-node-server/routes.js
@@ -42,4 +42,12 @@ const requestHandler = (req, res) => {
   res.end();
 };
 
-module.exports = requestHandler;
+// module.exports = requestHandler;
+
+// module.exports = {
+//   handler: requestHandler,
+//   someText: 'Some hard coded text'
+// };
+
+module.exports.handler = requestHandler;
+module.exports.someText = 'Some hard coded text';
