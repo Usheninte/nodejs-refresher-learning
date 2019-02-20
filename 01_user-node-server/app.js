d9dc29c28029111163b4b8949b577107cfe97734
Usheninte <jrandangana@gmail.com>
2019-02-20 14:43:14 +0100

    edited server

--- 01_user-node-server/app.js
+++ 01_user-node-server/app.js
@@ -1,7 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+  res.write('<html>')
+  res.write('<head><title>Userpool</title></head>')
   res.write('<h1>Hello Userpool</h1>');
+  res.write('</html>')
   res.end();
 });
 
