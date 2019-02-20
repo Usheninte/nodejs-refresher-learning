831531b8d22d1fe8aecc3914e2d0731f44c17a3f
Usheninte <jrandangana@gmail.com>
2019-02-20 15:26:38 +0100

    edited server

--- 01_user-node-server/app.js
+++ 01_user-node-server/app.js
@@ -1,11 +1,30 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.write('<html>')
-  res.write('<head><title>Userpool</title></head>')
-  res.write('<h1>Hello Userpool</h1>');
-  res.write('</html>')
-  res.end();
+  // PARSE url
+  const url = req.url;
+
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Userpool | Home/title></head>');
+    res.write('<h1>Hello Userpool</h1>');
+    res.write('</html>');
+    return res.end();
+  };
+
+  if (url === '/users') {
+    res.write('<html>');
+    res.write('<head><title>Userpool | List of Users</title></head>');
+    res.write('<ul>');
+    res.write('<li>User 1</li>')
+    res.write('<li>User 2</li>')
+    res.write('<li>User 3</li>')
+    res.write('<li>User 4</li>')
+    res.write('<li>User 5</li>')
+    res.write('</ul>');
+    res.write('</html>');
+    return res.end();
+  };
 });
 
 server.listen(3000);
