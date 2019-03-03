2777b1ca898d3a4b7bb85d97b5904ebd67813a35
Usheninte <jrandangana@gmail.com>
2019-03-03 05:52:42 +0100

    setup form

--- section_B/learning-express/app.js
+++ section_B/learning-express/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// Parsing MIDDLEWARE
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Route Handling MIDDLEWARE
 app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text name="title"><button type="submit">Add Product</button></form>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
 app.use('/product', (req, res, next) => {
