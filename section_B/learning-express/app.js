70ce55f4f047b8f68bb35002ea24d35c99fcb609
Usheninte <jrandangana@gmail.com>
2019-03-03 05:32:33 +0100

    trimmed file

--- section_B/learning-express/app.js
+++ section_B/learning-express/app.js
@@ -2,18 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('This always runs!');
-  next(); // this allows the request to continue to the next middleware
-});
-
 app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
   res.send('<form action="/product" method="POST"><input type="text name="title"><button type="submit">Add Product</button></form>');
 });
 
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
 app.use('/', (req, res, next) => {
-  console.log('Yet another middleware!');
   res.send('<h1>Hello from Express!</h1>');
 });
 
