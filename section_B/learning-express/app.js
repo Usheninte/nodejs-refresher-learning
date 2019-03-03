5b107b8749541bc5cb671d506ce746cdc894c0b6
Usheninte <jrandangana@gmail.com>
2019-03-03 01:04:06 +0100

    editions

--- section_B/learning-express/app.js
+++ section_B/learning-express/app.js
@@ -2,12 +2,19 @@ const express = require('express');
 
 const app = express();
 
+/*
 app.use((req, res, next) => {
   console.log('In the middleware!');
   next(); // this allows the request to continue to the next middleware
 });
+*/
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+  console.log('Yet another middleware!');
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
   console.log('In another middleware!');
   res.send('<h1>Hello from Express!</h1>');
 });
