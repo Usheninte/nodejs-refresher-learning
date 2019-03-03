b7b0f269725d71f14a9ea6028073be50a7da5508
Usheninte <jrandangana@gmail.com>
2019-03-03 02:39:49 +0100

    added distinct responses to different routes

--- section_B/project-express/app.js
+++ section_B/project-express/app.js
@@ -2,9 +2,21 @@ const express = require('express');
 
 const app = express();
 
+/*
 app.use('/users', (req, res, next) => {
   console.log('Users are Family');
   next();
+});
+
+app.use('/', (req, res, next) => {
+  console.log('Welcome Home!');
+  res.send('<h1>Home is where the code is!</h1>');
+});
+*/
+
+app.use('/users', (req, res, next) => {
+  console.log('Users are Family');
+  res.send('<h1><i>Love is when a user smiles</i> :)</h1>');
 });
 
 app.use('/', (req, res, next) => {
