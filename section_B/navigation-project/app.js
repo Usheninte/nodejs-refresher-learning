5ccded61039474fe7646151050772b4bcc75abf1
Usheninte <jrandangana@gmail.com>
2019-03-05 07:28:36 +0100

    implemented users nav

--- section_B/navigation-project/app.js
+++ section_B/navigation-project/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res, next) => {
-  res.send('<h1>Hey World!</h1>');
-});
+const navRoutes = require('./routes/navigation');
+
+app.use(navRoutes);
 
 app.listen(3000);
