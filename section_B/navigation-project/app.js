357706870af0cbc4fd29cfcc73b73ee73ca410f6
Usheninte <jrandangana@gmail.com>
2019-03-05 07:36:50 +0100

    added path

--- section_B/navigation-project/app.js
+++ section_B/navigation-project/app.js
@@ -1,8 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
 
 const navRoutes = require('./routes/navigation');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(navRoutes);
 
